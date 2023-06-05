b239f022d74f2041e46df5cefa15c79c273f2850
xeward-admin <135587719+xeward-admin@users.noreply.github.com>
2023-06-05 03:25:54 -0400

    Add files via upload

--- forms/quote.php
+++ forms/quote.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'hjjin@xeward.co.kr';
+  $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -21,7 +21,7 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->subject = 'Request for a quote';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -35,6 +35,7 @@
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['phone'], 'Phone');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
