abfaa0a69576c11f4c19b9e5d5ec78c523c7a107
xeward-admin <135587719+xeward-admin@users.noreply.github.com>
2023-06-05 03:47:04 -0400

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'hjjin@xeward.co.kr';
+  $receiving_email_address = 'jcho@xeward.co.kr';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
